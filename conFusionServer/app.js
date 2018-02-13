8b96acd103a80fd591f33c2727f066ea8d592521
Naumand Napuce <nnapuce@helius-software.com>
2018-02-13 22:30:04 +0100

    Assignment week 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -17,6 +17,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
@@ -70,6 +71,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
 
 // catch 404 and forward to error handler
