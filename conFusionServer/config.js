db08ab1ad089a92ee575093f7e801ce3f94099b0
Naumand Napuce <nnapuce@helius-software.com>
2018-02-13 21:37:30 +0100

    Facebook Passport

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2017553548530629',
+        clientSecret: 'ea60f876cd43a0041175784829e10f65'
+    }
 }
+
